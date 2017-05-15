1415fb3a7730bcf8a78e71d1ace1601648bb3721
ZaneWalter <mtayyabrashid.tr@gmail.com>
2017-05-15 12:24:27 +0500

    Update Main.php

--- src/kenygamer/ServerRules/Main.php
+++ src/kenygamer/ServerRules/Main.php
@@ -13,35 +13,107 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		$this->saveDefaultConfig();
-		$this->getLogger()->info(TEXTFORMAT::BLUE . "[ServerRules]" .TEXTFORMAT::RED. " >>" .TEXTFORMAT::AQUA.  "The plugin has been enabled.");
+		$this->getLogger()->info(TEXTFORMAT::BLUE . "[CE INFO]" .TEXTFORMAT::RED. " >>" .TEXTFORMAT::AQUA.  "The plugin has been enabled.");
 	}
 	public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
         $cmd = strtolower($command->getName());
         switch ($cmd){
-            case "rules":
+            case "ceinfo":
                 if (!($sender instanceof Player)){
-                    $sender->sendMessage(TEXTFORMAT::GOLD . "--------[ServerRules]--------");
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule1"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule2"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule3"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule4"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule5"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule6"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule7"));
-                    $sender->sendMessage(TEXTFORMAT::GREEN . "- " . $this->getConfig()->get("rule8"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-0"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-1"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-2"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-3"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-4"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-5"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-6"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-7"));
+	 	    $sender->sendMessage("" . $this->getConfig()->get("rule-8"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-9"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-10"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-11"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-12"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-13"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-14"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-15"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-16"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-17"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-18"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-19"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-20"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-21"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-22"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-23"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-24"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-25"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-26"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-27"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-28"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-29"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-30"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-31"));
+		    $sender->sendMessage("" . $this->getConfig()->get("rule-32"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-33"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-34"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-35"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-36"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-37"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-38"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-39"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-40"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-41"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-42"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-43"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-44"));
                     return true;
                 }
                 $player = $this->getServer()->getPlayer($sender->getName());
-                if ($player->hasPermission("command.rules")){
-                    $sender->sendMessage("--------[ServerRules]--------");
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule1"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule2"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule3"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule4"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule5"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule6"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule7"));
-                    $sender->sendMessage("- " . $this->getConfig()->get("rule8"));
+                if ($player->hasPermission("ceinfo.info")){
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-0"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-1"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-2"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-3"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-4"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-5"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-6"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-7"));
+	 	    $sender->sendMessage("" . $this->getConfig()->get("rule-8"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-9"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-10"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-11"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-12"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-13"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-14"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-15"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-16"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-17"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-18"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-19"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-20"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-21"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-22"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-23"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-24"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-25"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-26"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-27"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-28"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-29"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-30"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-31"));
+		    $sender->sendMessage("" . $this->getConfig()->get("rule-32"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-33"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-34"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-35"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-36"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-37"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-38"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-39"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-40"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-41"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-42"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-43"));
+                    $sender->sendMessage("" . $this->getConfig()->get("rule-44"));
                     return true;
                 }
                 break;
